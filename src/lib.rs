ffa19bf53dce77990a6a66aa976323be8949a7cd
doomed-doom <doomfinland@gmail.com>
2025-08-16 19:32:10 +0300

    More output info

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-use std::time::{Duration, Instant};
+use std::fmt::{Display, Formatter};
 use std::net::Ipv4Addr;
+use std::time::Instant;
 
 #[allow(dead_code)]
 pub struct PingStats {
-    host: Ipv4Addr,
-    count: u16,
-    transmitted: u16,
-    received: u16,
-    loss: u16,
-    start: Instant,
-    end: Instant,
-    total_delay: Duration,
+    pub host: Ipv4Addr,
+    pub transmitted: usize,
+    pub received: usize,
+    pub loss: usize,
+    pub start: Instant,
+    pub end: Instant,
+    pub avg_delay: f64,
+}
+
+impl Display for PingStats {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "\n--- Статистика пинга {} ---\nОтправлено: {} пакетов.\nПолучено: {}, потери: {}%, общее время: {:.4}\nСредняя задержка (rtt): {:.4}",
+            self.host,
+            self.transmitted,
+            self.received,
+            self.loss,
+            (self.end - self.start).as_secs_f64(),
+            self.avg_delay
+        )
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use socket2::{Socket, Domain, Type, Protocol};
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use clap::{Parser, value_parser, command};
-use std::thread;
-use std::time::Duration;
+use clap::{Parser, command, value_parser};
+use socket2::{Domain, Protocol, Socket, Type};
 use std::mem::MaybeUninit;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::thread;
+use std::time::{Duration, Instant};
+
 use ping::PingStats;
+
+mod consts;
+use consts::{ICMP_ECHO_ANSWER_TYPE, ICMP_ECHO_REQUEST_TYPE};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -16,7 +20,7 @@ struct CliArgs {
     #[arg(short, long, default_value = "0", value_parser = value_parser!(usize))]
     count: usize,
     /// Задержка (в сек.)
-    #[arg(short, long)]
+    #[arg(short, long, default_value = "1")]
     duration: Option<f64>,
 }
 
@@ -45,7 +49,14 @@ impl IcmpPacket {
     }
 }
 
-fn create_packet(packet_type: u8, packet_code: u8, packet_checksum: u16, packet_id: u16, packet_seq_num: u16, payload: Vec<u8>) -> Vec<u8>{
+fn create_packet(
+    packet_type: u8,
+    packet_code: u8,
+    packet_checksum: u16,
+    packet_id: u16,
+    packet_seq_num: u16,
+    payload: Vec<u8>,
+) -> Vec<u8> {
     let packet = IcmpPacket {
         icmp_type: packet_type,
         code: packet_code,
@@ -60,12 +71,12 @@ fn create_packet(packet_type: u8, packet_code: u8, packet_checksum: u16, packet_
 
 fn main() {
     let args = CliArgs::parse();
-    let input_addr: Ipv4Addr = args.ip;
+    let ip_addr: Ipv4Addr = args.ip;
     let count: usize = args.count;
     let dur: Duration = Duration::from_secs_f64(args.duration.unwrap());
 
     let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::ICMPV4)).unwrap();
-    let connect_addr = SocketAddr::new(IpAddr::from(input_addr), 0);
+    let connect_addr = SocketAddr::new(IpAddr::from(ip_addr), 0);
 
     socket.connect(&connect_addr.into()).unwrap();
 
@@ -74,31 +85,56 @@ fn main() {
     let mut send = 0;
     let mut recv = 0;
 
-    let packet_len: usize = create_packet(8, 0, 0, 1, 1, vec![0]).len();
+    let packet_len: usize = create_packet(ICMP_ECHO_REQUEST_TYPE, 0, 0, 1, 1, vec![0]).len();
 
-    println!("Начинаем пинг {} - {} байт данных.",
-        input_addr, packet_len
+    println!(
+        "Начинаем пинг {} - {} байт данных пакет.",
+        ip_addr, packet_len
     );
 
+    let mut ping_delays: Vec<Duration> = Vec::new();
+
+    let start = Instant::now();
     while count == 0 || send < count {
         let packet: Vec<u8> = create_packet(8, 0, 0, 1, 1, vec![0]);
+        let start_send = Instant::now();
         socket.send(&packet).unwrap();
         send += 1;
 
         let len = socket.recv(&mut buf).unwrap();
         let bytes: &[u8] = unsafe { std::slice::from_raw_parts(buf.as_ptr() as *const u8, len) };
 
-        if len > 0 && (bytes[1] == 0u8) {
-            println!("{} байт от {}",
-                len, input_addr
-            );
+        let end_send = Instant::now();
+        if len > 0 && (bytes[1] == ICMP_ECHO_ANSWER_TYPE) {
+            println!("{} байт от {}", len, ip_addr);
             recv += 1;
         }
+
+        ping_delays.push(end_send - start_send);
 
         if send != count {
             thread::sleep(dur);
         }
     }
-    println!("--- Статистика пинга {} ---", input_addr);
-    println!("Отправлено: {send} пакетов.\nПолучено: {recv}. Потери: {}%", (send - recv));
+    let end = Instant::now();
+
+    let avg_delay: f64 = {
+        let mut all_delay: f64 = 0.0;
+        for dur in &ping_delays {
+            all_delay += dur.as_secs_f64();
+        };
+        all_delay / ping_delays.len() as f64
+    };
+
+    let result = PingStats {
+        host: ip_addr,
+        transmitted: send,
+        received: recv,
+        loss: send - recv,
+        start: start,
+        end: end,
+        avg_delay: avg_delay,
+    };
+
+    println!("{result}");
 }
